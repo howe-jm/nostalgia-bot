436f20351d66140ba97dcdd4179468fb82095f67
Jason Howe <trisynth@gmail.com>
2021-01-29 21:18:02 -0500

    Basic bot chat functionality.

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('ANGULAR'):
+    await message.channel.send('REACT!!!!!!')
+
+  elif message.content.lower().startswith('angular'):
+    await message.channel.send('React!')
+
+  elif message.content.startswith('REACT'):
+    await message.channel.send('ANGULAR!!!!!')
+
+  elif message.content.lower().startswith('react'):
+    await message.channel.send('Angular!')
+    
+
+client.run(os.getenv('TOKEN'))

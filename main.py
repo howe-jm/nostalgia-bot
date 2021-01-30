cf52f131ccf3905c63253c3254fca45056d1bea3
Jason Howe <trisynth@gmail.com>
2021-01-30 01:29:35 -0500

    Testing code.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
+print(message.content)
 async def on_message(message):
   if message.author == client.user:
     return
